bdafc5d7c767de2a7e2f5dfad7ae7e816bb9d4a6
Nicol Acosta <nicol@landonearth.com>
2021-03-29 09:47:34 -0600

    fix details on corne via luna

--- keyboards/crkbd/keymaps/via_luna/rules.mk
+++ keyboards/crkbd/keymaps/via_luna/rules.mk
@@ -2,7 +2,7 @@ AUTO_SHIFT_ENABLE = yes
 OLED_DRIVER_ENABLE  = yes
 VIA_ENABLE = yes
 MOUSEKEY_ENABLE = no       # Mouse keys
-RGBLIGHT_ENABLE = yes      # Enable WS2812 RGB underlight. 
+# RGBLIGHT_ENABLE = yes      # Enable WS2812 RGB underlight. 
 
 # Luna config
 WPM_ENABLE = yes
